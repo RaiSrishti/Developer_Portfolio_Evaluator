3c0fb0a405b48b2ea7cc16506354500db53ff443
RaiSrishti <srishtirai1611@gmail.com>
2026-04-03 15:46:49 +0530

    Routing the profile Api

--- server/routes/profileRoutes.js
+++ server/routes/profileRoutes.js
@@ -2,6 +2,12 @@ const express = require("express");
 const router = express.Router();
 const { getProfile } = require("../controllers/profileController");
 
+// Root route for /api/profile
+router.get("/", (req, res) => {
+  res.json({ message: "Profile API is working. Use /api/profile/:username" });
+});
+
+// Get profile by username
 router.get("/:username", getProfile);
 
 module.exports = router;
